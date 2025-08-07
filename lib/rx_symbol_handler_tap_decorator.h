ab5eec478e4e9cdd3aa1b91ae03b5f7a27faaad4
Joaquim Broquetas <joaquim.broquetas@srs.io>
2025-08-07 14:31:33 +0200

    add external UL processor

--- lib/rx_symbol_handler_tap_decorator.h
+++ lib/rx_symbol_handler_tap_decorator.h
@@ -10,13 +10,15 @@
 
 #pragma once
 
+#include "external_ul_symbol_processor.h"
 #include "srsran/phy/support/prach_buffer_context.h"
 #include "srsran/phy/upper/upper_phy_rx_symbol_handler.h"
 #include "srsran/srslog/logger.h"
+#include "srsran/srslog/srslog.h"
 
 namespace srsran {
 
-/// Decorator for the upper PHY RX symbol handler the allows tapping into the received symbols.
+/// Decorator for the upper physical layer receive symbol handler the allows tapping into the received symbols.
 class upper_phy_rx_symbol_handler_tap_decorator : public upper_phy_rx_symbol_handler
 {
 public:
@@ -25,17 +27,27 @@ public:
 
   /// Constructor that takes ownership of an RX symbol handler.
   explicit upper_phy_rx_symbol_handler_tap_decorator(std::unique_ptr<upper_phy_rx_symbol_handler> handler_,
-                                                     srslog::basic_logger&                        logger_) :
-    handler(std::move(handler_)), logger(logger_)
+                                                     unsigned                                     nof_rb,
+                                                     unsigned                                     nof_ports,
+                                                     const std::string&                           processor_arguments) :
+    handler(std::move(handler_)),
+    processor(nof_rb, nof_ports, processor_arguments),
+    logger(srslog::fetch_basic_logger("PHY_TAP", true))
   {
     srsran_assert(handler, "Invalid Rx symbol handler.");
+
+    // TODO: Set the log level based on the configuration arguments.
+    logger.set_level(srslog::basic_levels::debug);
   }
 
   // See the interface for documentation.
   void
   handle_rx_symbol(const upper_phy_rx_symbol_context& context, const shared_resource_grid& grid, bool is_valid) override
   {
-    // Handle Rx symbol.
+    // Apply the external processing.
+    processor.process(context, grid);
+
+    // Pass the modified grid to the RX symbol handler.
     handler->handle_rx_symbol(context, grid, is_valid);
 
     logger.debug("Received symbol: sector {}, slot {}, symbol {}, is_valid: {}",
@@ -54,8 +66,10 @@ public:
   }
 
 private:
-  /// An instance of the upper PHY RX symbol handler.
+  /// An instance of the upper physical layer receive symbol handler.
   std::unique_ptr<upper_phy_rx_symbol_handler> handler;
+  /// UL symbol processor for processing the received symbols.
+  external_ul_symbol_processor processor;
   /// Logger object.
   srslog::basic_logger& logger;
 };
--- lib/rx_symbol_handler_tap_factory.cpp
+++ lib/rx_symbol_handler_tap_factory.cpp
@@ -24,38 +24,47 @@ public:
   ///
   /// It takes an RX symbol handler factory that is used to create RX symbol handler instances. Each instance is
   /// injected into the tap decorator upon creation.
-  explicit upper_phy_rx_symbol_handler_tap_factory_impl(std::unique_ptr<upper_phy_rx_symbol_handler_factory> factory_,
-                                                        srslog::basic_logger&                                logger_) :
-    rx_symbol_handler_factory(std::move(factory_)), logger(logger_)
+  explicit upper_phy_rx_symbol_handler_tap_factory_impl(std::shared_ptr<upper_phy_rx_symbol_handler_factory> factory_,
+                                                        unsigned                                             nof_rb_,
+                                                        unsigned                                             nof_ports_,
+                                                        const std::string& processor_arguments_) :
+    base_factory(std::move(factory_)), nof_rb(nof_rb_), nof_ports(nof_ports_), processor_arguments(processor_arguments_)
   {
-    srsran_assert(rx_symbol_handler_factory, "Invalid Rx symbol handler factory.");
+    srsran_assert(base_factory, "Invalid Rx symbol handler factory.");
   }
 
-  /// Creates a new upper PHY RX symbol handler tap decorator.
+  /// Creates a new upper physical layer receive symbol handler tap decorator.
   std::unique_ptr<upper_phy_rx_symbol_handler> create(uplink_slot_processor_pool& ul_processor_pool_) override
   {
     // Create the RX symbol handler.
-    std::unique_ptr<upper_phy_rx_symbol_handler> rx_symbol_handler =
-        rx_symbol_handler_factory->create(ul_processor_pool_);
+    std::unique_ptr<upper_phy_rx_symbol_handler> rx_symbol_handler = base_factory->create(ul_processor_pool_);
 
     // Create and return the RX symbol handler tap decorator.
-    return std::make_unique<upper_phy_rx_symbol_handler_tap_decorator>(std::move(rx_symbol_handler), logger);
+    return std::make_unique<upper_phy_rx_symbol_handler_tap_decorator>(
+        std::move(rx_symbol_handler), nof_rb, nof_ports, processor_arguments);
   }
 
 private:
   /// Factory that creates RX symbol handlers.
-  std::unique_ptr<upper_phy_rx_symbol_handler_factory> rx_symbol_handler_factory;
-  /// Logger object.
-  srslog::basic_logger& logger;
+  std::shared_ptr<upper_phy_rx_symbol_handler_factory> base_factory;
+  /// Number of resource blocks to process in the resource grid.
+  unsigned nof_rb;
+  /// Number of ports to process.
+  unsigned nof_ports;
+  /// External processor arguments.
+  std::string processor_arguments;
 };
 
 } // namespace
 
-/// \brief Creates a RX symbol handler factory.
-std::unique_ptr<upper_phy_rx_symbol_handler_factory>
-srsran::create_rx_symbol_handler_tap_factory(std::unique_ptr<upper_phy_rx_symbol_handler_factory> factory,
-                                             srslog::basic_logger&                                logger_)
+// See interface for documentation.
+std::shared_ptr<upper_phy_rx_symbol_handler_factory>
+srsran::create_rx_symbol_handler_tap_factory(std::shared_ptr<upper_phy_rx_symbol_handler_factory> factory,
+                                             unsigned                                             nof_rb,
+                                             unsigned                                             nof_ports,
+                                             const std::string&                                   processor_arguments)
 {
   // Create and return the RX symbol handler tap factory.
-  return std::make_unique<upper_phy_rx_symbol_handler_tap_factory_impl>(std::move(factory), logger_);
+  return std::make_shared<upper_phy_rx_symbol_handler_tap_factory_impl>(
+      std::move(factory), nof_rb, nof_ports, processor_arguments);
 }
