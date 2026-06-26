294386fdfec723a4efbf594c3531b7d8aa11a8cf
Jiaxun Yang <jiaxun.yang@flygoat.com>
2026-06-26 03:22:30 +0100

    Docker: patch DPDK ACC100 VFIO interrupts

--- docker/scripts/build_dpdk.sh
+++ docker/scripts/build_dpdk.sh
@@ -6,6 +6,22 @@
 
 set -e # stop executing after error
 
+apply_dpdk_patches() {
+    local dpdk_version=$1
+    local script_dir=$2
+    local patch_dir="${DPDK_PATCH_DIR:-${script_dir}/../patches/dpdk/${dpdk_version}}"
+
+    [ ! -d "$patch_dir" ] && return
+
+    shopt -s nullglob
+    local patch_file
+    for patch_file in "$patch_dir"/*.patch; do
+        echo "Applying DPDK patch: ${patch_file}"
+        patch -p1 < "$patch_file"
+    done
+    shopt -u nullglob
+}
+
 main() {
     # Check number of args
     if [ $# -lt 1 ] || [ $# -gt 3 ]; then
@@ -17,10 +33,14 @@ main() {
     local dpdk_version=$1
     local arch="${2:--Dcpu_instruction_set=native}"
     local ncores="${3:-$(nproc)}"
+    local script_dir
+
+    script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
     cd /tmp
     curl -L "https://fast.dpdk.org/rel/dpdk-${dpdk_version}.tar.xz" | tar xJf -
     cd dpdk*"${dpdk_version}"
+    apply_dpdk_patches "${dpdk_version}" "${script_dir}"
     read -ra arch_args <<< "${arch}"
     meson setup build --prefix "/opt/dpdk/${dpdk_version}" "${arch_args[@]}"
     meson compile -j "${ncores}" -C build
