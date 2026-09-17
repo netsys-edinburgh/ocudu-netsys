2df63752b5c3ec7b65254fe69a7cc5fb82476b7a
Francisco Paisana <francisco.paisana@srs.io>
2026-09-18 01:12:08 +0200

    sched: drop contradictory empty-VRB assert in set_pusch_params
    set_pusch_params asserted that the VRB interval was not empty, then a few
    lines below handled the empty case by cancelling the grant. The assert
    fired before that handler could ever run, while the intra-slice scheduler
    calls set_pusch_params({}) on purpose whenever no VRBs could be found.
    
    Keep the cancel path and drop the assert, matching set_pdsch_params.
    
    Signed-off-by: Francisco Paisana <francisco.paisana@srs.io>

--- lib/scheduler/ue_scheduling/ue_cell_grid_allocator.cpp
+++ lib/scheduler/ue_scheduling/ue_cell_grid_allocator.cpp
@@ -773,8 +773,6 @@ ue_cell_grid_allocator::setup_ul_grant_builder(const slice_ue&                  
 
 void ue_cell_grid_allocator::set_pusch_params(ul_grant_info& grant, const vrb_interval& vrbs) const
 {
-  ocudu_assert(not vrbs.empty(), "Invalid set of PUSCH VRBs");
-
   ue&      u     = ues[grant.user->ue_index()];
   ue_cell& ue_cc = *u.find_cell(cell_alloc.cell_index());
 
